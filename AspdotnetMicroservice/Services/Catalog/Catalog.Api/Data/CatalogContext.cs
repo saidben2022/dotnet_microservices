bb1776afeac1340e3189a7e5fb9147b0e762ae4a
said benbouchta <saidbenbouchta93@gmail.com>
2022-06-14 09:06:28 +0100

    Docker Compose and mongo db

--- AspdotnetMicroservice/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
+++ AspdotnetMicroservice/Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -9,9 +9,9 @@ namespace Catalog.Api.Data
         public IMongoCollection<Product> Products { get; }
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetValue<string>("ProductDatabase:ConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("ProductDatabase:DatabaseName"));
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("ProductDatabase:CollectionName"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
             CatalogContextSeed.SeedData(Products);
         }
       
